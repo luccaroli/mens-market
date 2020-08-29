2bf8d72bc1eb5f4486d2f7f05c9338c6ea6a2f0f
luccaroli <luccaroli20@gmail.com>
2020-08-29 16:39:50 -0300

    feature:nodemailer

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,6 +7,8 @@ import Notification from '../../app/schemas/Notification'
 import User from '../models/User'
 import File from '../models/File'
 
+import Mail from '../../lib/Mail'
+
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query
@@ -112,7 +114,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id)
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email']
+        }
+      ]
+    })
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -132,11 +142,15 @@ class AppointmentController {
 
     await appointment.save()
 
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Você tem um novo cancelamento'
+    })
+
     return res.json(appointment)
   }
-    
-
-
+  
 }
 
 export default new AppointmentController()
