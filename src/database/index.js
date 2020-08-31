bd0286f89cb2fda05da43c17a83faa6e3e6d8718
luccaroli <luccaroli20@gmail.com>
2020-08-31 03:21:14 -0300

    add environment variables

--- src/database/index.js
+++ src/database/index.js
@@ -23,8 +23,7 @@ class Database {
       .map(model => model.associate && model.associate(this.connection.models))
   }
   mongo() {
-    this.mongoConnection = mongoose.connect(
-      'mongodb://localhost:27017/gobarber',
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, 
       { useNewUrlParser: true, useFindAndModify: true }
     )
   }
